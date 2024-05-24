6c5c1eb1fb24ed753d71e781e17be299bf662170
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-05-24 19:32:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+module "vpc" {
+  source          = "/home/ubuntu/terraform/module/vpc"
+  vpc_cidr_block = var.vpc_cidr_block
+  vpc_tags       = var.vpc_tags
+}
 
+module "subnets" {
+  source = "/home/ubuntu/terraform/module/subnet"
+
+  vpc_id            = module.vpc.vpc_id
+  subnet_cidr_blocks = var.subnet_cidr_blocks
+  azs                = var.azs
+  subnet_tags        = var.subnet_tags
+}
