1d5459c946a0503180336b8ce57602071f6d618a
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-05-24 19:32:51 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 
+  required_version = ">= 1.2.0"
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region     = "us-east-1"  # Región de AWS en la que deseas operar
+  access_key = "A"
+  secret_key = "E"
+}
