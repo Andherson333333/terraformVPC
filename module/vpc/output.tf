45c673b3dc708c2b151951aef22a8027fd7b9d81
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-05-24 19:34:41 -0400

    Update output.tf

--- module/vpc/output.tf
+++ module/vpc/output.tf
@@ -1,1 +1,14 @@
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.web_server.id
+}
 
+output "vpc_cidr_block" {
+  description = "CIDR block of the created VPC"
+  value       = aws_vpc.web_server.cidr_block
+}
+
+output "vpc_tags" {
+  description = "Tags of the created VPC"
+  value       = aws_vpc.web_server.tags
+}
