73e569e4da8529ffadec7b7bd2d7745051116448
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-05-24 19:33:11 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,24 @@
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
 
+variable "vpc_tags" {
+  description = "Tags for the VPC"
+  type        = map(string)
+}
+
+variable "subnet_cidr_blocks" {
+  description = "CIDR blocks for the subnets"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "Availability Zones for the subnets"
+  type        = list(string)
+}
+
+variable "subnet_tags" {
+  description = "Tags for the subnets"
+  type        = map(string)
+}
