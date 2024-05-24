b01e5f2fddd201beaf67f218bd01ee65497fab20
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-05-24 19:35:32 -0400

    Update main.tf

--- module/subnet/main.tf
+++ module/subnet/main.tf
@@ -1,1 +1,10 @@
-
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.subnet_cidr_blocks)
+  vpc_id            = var.vpc_id
+  cidr_block        = element(var.subnet_cidr_blocks, count.index)
+  availability_zone = element(var.azs, count.index)
+  
+  tags = merge(var.subnet_tags, {
+    Name = "Public Subnet ${count.index + 1}"
+  })
+}
