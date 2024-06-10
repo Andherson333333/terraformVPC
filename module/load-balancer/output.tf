0bc89887a4b5e51498d584382b9e4c8fca75cca7
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-10 17:55:15 -0400

    Update output.tf

--- module/load-balancer/output.tf
+++ module/load-balancer/output.tf
@@ -2,3 +2,21 @@ output "target_group_id" {
   description = "ID del grupo de destino creado"
   value       = aws_lb_target_group.target_group.id
 }
+
+# Listener
+
+output "load_balancer_arn" {
+  description = "ARN del load balancer creado"
+  value       = aws_lb.load-balancer.arn
+}
+
+output "target_group_arn" {
+  description = "ARN del grupo de destino creado"
+  value       = aws_lb_target_group.target_group.arn
+}
+
+output "listener_arn" {
+  description = "ARN del listener creado"
+  value       = aws_lb_listener.listener.arn
+}
+
