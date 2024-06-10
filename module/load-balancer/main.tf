01f1b935c535e00124e679ed9597f531af20e693
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-10 17:03:25 -0400

    Update main.tf

--- module/load-balancer/main.tf
+++ module/load-balancer/main.tf
@@ -1,1 +1,27 @@
+resource "aws_lb" "load-balancer" {
+  name               = var.lb_name
+  internal           = var.lb_internal
+  load_balancer_type = var.lb_type
+  security_groups    = [var.securityGroupLB_id]  # Cambiar 'security_group' a 'security_groups'
 
+  subnets = var.lb_subnets
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name     = var.target_group_name
+  port     = var.target_group_port
+  protocol = var.target_group_protocol
+  vpc_id   = var.vpc_id  # Este campo necesita la declaración de la variable vpc_id
+
+  health_check {
+    path                = var.health_check_path
+    port                = var.health_check_port
+    protocol            = var.health_check_protocol
+    timeout             = var.health_check_timeout
+    interval            = var.health_check_interval
+    healthy_threshold   = var.health_check_healthy_threshold
+    unhealthy_threshold = var.health_check_unhealthy_threshold
+  }
+
+  tags = var.tags
+}
