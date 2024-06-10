aad45c2d5cbc7c2ed343162a299b7991db1c389f
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-10 17:00:25 -0400

    Update outuput.tf

--- module/subnetPrivate/outuput.tf
+++ module/subnetPrivate/outuput.tf
@@ -1,1 +1,19 @@
+output "private_subnet_ids" {
+  description = "IDs of the created private subnets"
+  value       = aws_subnet.private_subnets[*].id
+}
 
+output "private_subnet_cidr_blocks" {
+  description = "CIDR blocks of the created private subnets"
+  value       = aws_subnet.private_subnets[*].cidr_block
+}
+
+output "private_subnet_azs" {
+  description = "Availability Zones of the created private subnets"
+  value       = aws_subnet.private_subnets[*].availability_zone
+}
+
+output "private_subnet_tags" {
+  description = "Tags of the created private subnets"
+  value       = aws_subnet.private_subnets[*].tags
+}
