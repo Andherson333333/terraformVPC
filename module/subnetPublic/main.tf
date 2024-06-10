75fa7eb3ac29219d06c5de8cda5fc1e120d2cab6
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-10 16:58:17 -0400

    Update main.tf

--- module/subnetPublic/main.tf
+++ module/subnetPublic/main.tf
@@ -1,1 +1,42 @@
+# Creacion subnet
+resource "aws_subnet" "public_subnets" {
+  count             = length(var.public_subnet_cidr_blocks)
+  vpc_id            = var.vpc_id
+  cidr_block        = element(var.public_subnet_cidr_blocks, count.index)
+  availability_zone = element(var.public_azs, count.index)
+
+  tags = merge(var.public_subnet_tags, {
+    Name = "Public Subnet ${count.index + 1}"
+  })
+}
+
+# Creación de IGW
+resource "aws_internet_gateway" "gw" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "Nuevo-IGW"
+  }
+}
+
+# Creación de tabla pública
+resource "aws_route_table" "public_rt" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "2nd Route Table"
+  }
+}
+
+# Creación de la asociación de tabla de rutas con subredes públicas
+resource "aws_route_table_association" "public_subnet_asso" {
+  count          = length(aws_subnet.public_subnets[*].id)
+  subnet_id      = aws_subnet.public_subnets[count.index].id
+  route_table_id = aws_route_table.public_rt.id
+}
 
