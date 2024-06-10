3813c3705ab52a3e0f0c96ca35612f8d055178ab
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-10 17:00:04 -0400

    Update main.tf

--- module/subnetPrivate/main.tf
+++ module/subnetPrivate/main.tf
@@ -1,1 +1,63 @@
+# Creacion de subnet privadas
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnet_cidr_blocks)
+  vpc_id            = var.vpc_id
+  cidr_block        = element(var.private_subnet_cidr_blocks, count.index)
+  availability_zone = element(var.private_azs, count.index)
+
+  tags = merge(var.private_subnet_tags, {
+    Name = "Private Subnet ${count.index + 1}"
+  })
+}
+
+
+# Creación de tablas de rutas para subredes privadas
+resource "aws_route_table" "private_rt" {
+  count  = length(var.private_subnet_cidr_blocks)
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "Private Route Table ${count.index + 1}"
+  }
+}
+
+
+# Asociacion automatica por nombres
+resource "aws_route_table_association" "private_subnet_asso" {
+  for_each = { for idx, subnet in aws_subnet.private_subnets : idx => subnet if contains(keys(subnet.tags), "Name") }
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_rt[each.key].id
+#  depends_on     = [aws_route_table.private_rt[each.key]]
+}
+# Creacion de subnet privadas
+resource "aws_subnet" "private_subnets" {
+  count             = length(var.private_subnet_cidr_blocks)
+  vpc_id            = var.vpc_id
+  cidr_block        = element(var.private_subnet_cidr_blocks, count.index)
+  availability_zone = element(var.private_azs, count.index)
+
+  tags = merge(var.private_subnet_tags, {
+    Name = "Private Subnet ${count.index + 1}"
+  })
+}
+
+
+# Creación de tablas de rutas para subredes privadas
+resource "aws_route_table" "private_rt" {
+  count  = length(var.private_subnet_cidr_blocks)
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "Private Route Table ${count.index + 1}"
+  }
+}
+
+
+# Asociacion automatica por nombres
+resource "aws_route_table_association" "private_subnet_asso" {
+  for_each = { for idx, subnet in aws_subnet.private_subnets : idx => subnet if contains(keys(subnet.tags), "Name") }
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_rt[each.key].id
+#  depends_on     = [aws_route_table.private_rt[each.key]]
+}
 
