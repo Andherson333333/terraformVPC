7d814badb69b884395ce8579ceea1560d053908c
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-10 16:58:38 -0400

    Update output.tf

--- module/subnetPublic/output.tf
+++ module/subnetPublic/output.tf
@@ -1,1 +1,25 @@
+output "public_subnet_ids" {
+  description = "IDs of the created public subnets"
+  value       = aws_subnet.public_subnets[*].id
+}
+
+output "public_subnet_cidr_blocks" {
+  description = "CIDR blocks of the created public subnets"
+  value       = aws_subnet.public_subnets[*].cidr_block
+}
+
+output "public_subnet_azs" {
+  description = "Availability Zones of the created public subnets"
+  value       = aws_subnet.public_subnets[*].availability_zone
+}
+
+output "public_subnet_tags" {
+  description = "Tags of the created public subnets"
+  value       = aws_subnet.public_subnets[*].tags
+}
+
+output "public_subnet_ids_list" {
+  description = "List of IDs of the created public subnets"
+  value       = [for subnet in aws_subnet.public_subnets : subnet.id]
+}
 
