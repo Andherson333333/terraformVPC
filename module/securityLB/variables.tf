49753a7e3617c9ccf41cbbd1023c62e4b7af01b2
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-10 17:02:29 -0400

    Update variables.tf

--- module/securityLB/variables.tf
+++ module/securityLB/variables.tf
@@ -1,1 +1,19 @@
+variable "lb_security_group_name" {
+  description = "Nombre del security group para el load balancer"
+  type        = string
+}
 
+variable "lb_security_group_description" {
+  description = "Descripción del security group para el load balancer"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID de la VPC donde se creará el security group"
+  type        = string
+}
+
+variable "exposed_ports" {
+  description = "Puertos expuestos por el load balancer"
+  type        = list(number)
+}
