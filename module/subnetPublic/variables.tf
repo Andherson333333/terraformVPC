9290c0f8d4a7d0669e3fa161a46ffd4cdb0e7412
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-10 16:58:58 -0400

    Update variables.tf

--- module/subnetPublic/variables.tf
+++ module/subnetPublic/variables.tf
@@ -1,1 +1,20 @@
+variable "vpc_id" {
+  description = "ID of the VPC where the subnets will be created"
+  type        = string
+}
+
+variable "public_subnet_cidr_blocks" {
+  description = "CIDR blocks for the subnets"
+  type        = list(string)
+}
+
+variable "public_azs" {
+  description = "Availability Zones for the subnets"
+  type        = list(string)
+}
+
+variable "public_subnet_tags" {
+  description = "Tags for the subnets"
+  type        = map(string)
+}
 
