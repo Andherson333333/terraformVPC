a24758bebebda4d08534d6bedb28e0d882041fc9
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-29 22:38:52 -0400

    Update variables.tf

--- module/securityEC2/variables.tf
+++ module/securityEC2/variables.tf
@@ -1,1 +1,25 @@
+variable "ec2_security_group_name" {
+  description = "Nombre del security group para el load balancer"
+  type        = string
+}
+
+variable "ec2_security_group_description" {
+  description = "Descripción del security group para el load balancer"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "ID de la VPC donde se creará el security group"
+  type        = string
+}
+
+variable "exposed_ports_ec2" {
+  description = "Puertos expuestos por el load balancer"
+  type        = list(number)
+}
+
+variable "securityGroupLB_id" {
+  description = "ID del security group para el load balancer"
+  type        = string
+}
 
