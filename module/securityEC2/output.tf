fd22d3d07b133d7024bf5fb0091b98a6f14869ae
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-06-29 22:39:11 -0400

    Update output.tf

--- module/securityEC2/output.tf
+++ module/securityEC2/output.tf
@@ -1,1 +1,10 @@
+output "security_groupEC2_id" {
+  description = "ID del Security Group creado para instancias EC2"
+  value       = aws_security_group.instancia_ec2.id
+}
+
+#output "launch_config_id" {
+#  description = "ID de la Launch Configuration para instancias EC2"
+#  value       = aws_launch_configuration.ec2_config.id
+#}
 
